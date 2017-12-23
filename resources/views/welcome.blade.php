b1dc0f2a126e23f85eb83f82187719653c7ae166
Sean <hello@seanoneill.me>
2017-12-23 22:15:30 +0000

    metadata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('description', 'A Place to get simple explanations of coding terms.')
+@section('description', 'CodingDictionary is a place with simple, easy to understand explanations of coding terms or add your own explanation in under 255 characters.')
 @section('content')
 <div class="container">
     <div class="row">
@@ -9,7 +9,7 @@
 
          <div class="panel-body">
     <div class="search-box-frontpage text-center">
-      <h4>A Place to get simple explanations of coding terms.</h4>
+      <h4>A place to get simple explanations of coding terms.</h4>
      <form role="form" class="search" method="POST" action="{{ route('search') }}">
                {{ csrf_field() }}
          <h3 class="no-margin-top h3"></h3>
