2f296eb9d3e2b7d554a0a230e304357a0e759d16
Sean <hello@seanoneill.me>
2017-12-25 22:39:12 +0000

    can access all words page without login now

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -14,10 +14,6 @@ class WordController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
