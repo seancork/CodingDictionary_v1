a676b3d80054ec1187ddeec9efba55e80ca89126
Sean <hello@seanoneill.me>
2018-01-16 00:11:26 +0000

    testing get user ip address

--- vendor/laravel/framework/src/Illuminate/Http/Request.php
+++ vendor/laravel/framework/src/Illuminate/Http/Request.php
@@ -260,24 +260,9 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
      */
     public function ip()
     {
-       // return $this->getClientIp();
-
-        $test = "192.3434.3434";
-         return $test;
+        return $this->getClientIp();
     }
 
-    public function getIp(){
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-        if (array_key_exists($key, $_SERVER) === true){
-            foreach (explode(',', $_SERVER[$key]) as $ip){
-                $ip = trim($ip); // just to be safe
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                    return $ip;
-                }
-            }
-        }
-    }
-}
     /**
      * Get the client IP addresses.
      *
